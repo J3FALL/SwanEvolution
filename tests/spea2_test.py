dd27da86908d432b273079960a6fa44da1c912cf
J3FALL <crisp.coad@gmail.com>
2019-01-04 19:42:34 +0300

    Add Zitzler–Deb–Thiele's function N. 1 benchmark

--- tests/spea2_test.py
+++ tests/spea2_test.py
@@ -1,4 +1,5 @@
 from src.algorithm.benchmarks import schaffer
+from src.algorithm.benchmarks import zdt
 from src.algorithm.spea2 import SPEA2
 
 
@@ -16,6 +17,20 @@ class SPEA2Test:
         last_variance = history.last().pop_variance
 
         assert is_less_than(last_variance, 10)
+
+    def test_zitzler_deb_thiele_function_optimization(self):
+        alg = SPEA2(
+            params=SPEA2.Params(max_gens=200, pop_size=50, archive_size=25, crossover_rate=0.6, mutation_rate=0.3),
+            new_individ=zdt.new_individ,
+            objectives=zdt.objectives,
+            crossover=zdt.crossover,
+            mutation=zdt.mutation,
+            pop_variance=schaffer.objectives_sum)
+
+        history = alg.solution()
+        last_variance = history.last().pop_variance
+
+        assert is_less_than(last_variance, 5)
 
 
 def is_less_than(values, limit):
